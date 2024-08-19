1201efa42ef5d15e78f76c9934c370416911c5da
Anna Ståhlberg <as228gn@student.lnu.se>
2024-08-19 12:42:30 +0200

    Adding picture API

--- index.js
+++ index.js
@@ -1,8 +1,11 @@
 const send = document.getElementById('send')
 const input = document.getElementById('name')
 const greeting = document.getElementById('greeting')
+const picture = document.getElementById('picture')
 
 send.addEventListener('click', () => {
   const nameInput = input.value
-  greeting.textContent = 'Hej, ' + nameInput + '!'
+  greeting.textContent = 'Hello, ' + nameInput + ' here is your picture!'
+  picture.src = 'https://picsum.photos/400/500?'
+  input.value = ''
 })
