8458e3492c6631638a1fed4adfc5667be161487b
Anna Ståhlberg <as228gn@student.lnu.se>
2024-09-02 13:09:11 +0200

    Checking to see if my commits are going to github

--- index.js
+++ index.js
@@ -5,8 +5,9 @@ const picture = document.getElementById('picture')
 
 function getName(){
   const nameInput = input.value
+  const random = Math.random()
   greeting.textContent = 'Hello, ' + nameInput + ' here is a picture for you!'
-  picture.src = 'https://picsum.photos/400/500?'
+  picture.src = 'https://picsum.photos/400/500?' + random
   input.value = ''
 }
 
